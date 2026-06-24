9fedb7b4850fd8dc0266f5ffa06c08fb9b3e3672
Annie <anniehastoriya@gmail.com>
2026-06-24 09:31:56 -0500

    send

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,9 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS playdates;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS ratings;
+DROP TABLE IF EXISTS externalEvents; 
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
