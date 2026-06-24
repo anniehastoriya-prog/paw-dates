fe857a3e6c713c71d864920c6b63d10a210b294d
Annie <anniehastoriya@gmail.com>
2026-06-24 12:11:03 -0500

    Adding the playdates table in schema

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,11 @@ CREATE TABLE users (
 );
 
 CREATE TABLE playdates (
-  
-);
+  id SERIAL PRIMARY KEY,
+  request_dog_id INTEGER NOT NULL REFERENCES dog(id) ON DELETE CASCADE,
+  recipient_dog_id INTEGER NOT NULL REFERENCES dog(id) ON DELETE CASCADE,
+  timeslot DATE NOT NULL,
+  status text NOT NULL DEFAULT 'pending' CHECK ( status IN ('pending', 'confirmed', 'declined','cancelled')),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 
+);
